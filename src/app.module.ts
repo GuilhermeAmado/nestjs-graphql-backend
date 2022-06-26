a92c66457f77024167f59ed06ae22511435d8757
Guilherme Amado <guiii@live.com>
2022-06-25 21:56:55 -0300

    add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,9 @@ import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { join } from 'path';
 import { Restaurant } from './restaurants/entities/restaurant.entity';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { UsersModule } from './users/users.module';
+import { CommonModule } from './common/common.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
@@ -29,11 +32,13 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       database: process.env.DB_NAME,
       synchronize: true,
       logging: true,
-      entities: [Restaurant],
+      entities: [Restaurant, User],
       subscribers: [],
       migrations: [],
     }),
     RestaurantsModule,
+    UsersModule,
+    CommonModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/users/users.resolver.ts
@@ -0,0 +1,13 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+
+@Resolver(() => User)
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Query(() => Boolean)
+  create() {
+    return true;
+  }
+}
